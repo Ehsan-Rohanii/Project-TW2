684c6ccefad11fbc2def2f59eae6942a78586ff7
nrgs-rahimi <nrgs.rahimi82@gmail.com>
2025-12-12 10:23:57 -0800

    add product to page two

---
+++ Page-2/js/script.js
@@ -0,0 +1,6 @@
+'use strict'
+const hamberIcon = document.querySelector('.categories');
+const hamberMenu = document.querySelector('.hambergeri');
+hamberIcon.addEventListener('click', () => {
+    hamberMenu.classList.toggle('active-menu');
+});
